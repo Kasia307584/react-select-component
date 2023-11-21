73dcf09581b4e66db03a2711e6dd260f7282ef10
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-11-21 16:27:06 +0100

    Bundle file updated
    Bundle file updated by running npm run build

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,28 +2,69 @@
 
 var React = require('react');
 
+function _extends() {
+  _extends = Object.assign ? Object.assign.bind() : function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+    return target;
+  };
+  return _extends.apply(this, arguments);
+}
+
+/**
+ * MySelect component for creating a customizable dropdown select input.
+ *
+ * @param {Object} props - Component props.
+ * @param {Array} props.options - An array of objects representing the options available in the select dropdown.
+ * @param {Function} props.onChange - A callback function called when the selected value changes.
+ * @param {string} [props.label] - A label for the select input (optional).
+ * @returns {JSX.Element} MySelect component.
+ */
 function MySelect(_ref) {
   let {
     options,
     onChange,
     label
   } = _ref;
+  // State to track the selected value
   const [selectedValue, setSelectValue] = React.useState("");
+
+  /**
+   * Effect hook to notify the parent component when the selected value changes.
+   */
   React.useEffect(() => {
-    const selectedOption = options.filter(option => option.value === selectedValue);
-    onChange(selectedOption[0]);
+    // Find the selected option based on the value
+    const selectedOption = options.find(option => option.value === selectedValue);
+
+    // Call the onChange callback with the selected option
+    onChange(selectedOption);
   }, [selectedValue]);
+
+  /**
+   * Capitalize the first letter of a word.
+   *
+   * @param {string} word - The word to capitalize.
+   * @returns {string} Capitalized word.
+   */
   function capitalize(word) {
     return word[0].toUpperCase() + word.substring(1).toLowerCase();
   }
   return /*#__PURE__*/React.createElement(React.Fragment, null, label && /*#__PURE__*/React.createElement("label", {
     htmlFor: label
-  }, capitalize(label)), /*#__PURE__*/React.createElement("select", {
-    "aria-label": `select option`,
+  }, capitalize(label)), /*#__PURE__*/React.createElement("select", _extends({}, label && {
+    id: label
+  }, {
+    "aria-label": `Select option`,
     className: "form-select",
     value: selectedValue,
     onChange: e => setSelectValue(e.target.value)
-  }, options.map((option, index) => /*#__PURE__*/React.createElement("option", {
+  }), options.map((option, index) => /*#__PURE__*/React.createElement("option", {
     key: index,
     value: option.value
   }, option.label))));
